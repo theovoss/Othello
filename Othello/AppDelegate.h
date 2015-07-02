35a9a574c114cd572f6c908c62aa1fb70e661c2c
Theo Voss <theo.voss973@gmail.com>
2015-07-01 20:56:07 -0400

    Initial Commit

---
+++ Othello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Othello
+//
+//  Created by Theo Voss on 7/1/15.
+//  Copyright (c) 2015 Theo Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Othello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Othello
+//
+//  Created by Theo Voss on 7/1/15.
+//  Copyright (c) 2015 Theo Voss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
